0521ffa431a8298b970784fd061d006a73c43d85
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-04-19 01:44:07 -0300

    html pages. to-do: pass it to a template engine

--- server/Server.js
+++ server/Server.js
@@ -1,12 +1,14 @@
 import fastify from "fastify";
-import { CreateUser } from "./routes/users/CreateUser.js";
 import { connectDb } from "./db/mongodb.js";
+import { CreateUser } from "./routes/users/CreateUser.js";
 import { GetUsers } from "./routes/users/GetUsers.js";
 import { GetUser } from "./routes/users/GetUser.js";
 import { DeleteUser } from "./routes/users/DeleteUser.js";
 import { AuthUser } from "./routes/users/AuthUser.js";
+import fastifyFormbody from "@fastify/formbody";
 
 const app = fastify();
+app.register(fastifyFormbody)
 
 //routes
 app.register(CreateUser)
@@ -14,6 +16,7 @@ app.register(GetUsers)
 app.register(GetUser)
 app.register(DeleteUser)
 app.register(AuthUser)
+
 
 const start = async () => {
   try {
